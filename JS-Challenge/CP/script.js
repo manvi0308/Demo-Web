eca4bf6492898997644f4dddf227bcf7ced6c9df
Manvi Chaddha <60390722+manvi0308@users.noreply.github.com>
2021-10-13 13:09:06 +0000

    commit on main

---
+++ JS-Challenge/CP/script.js
@@ -0,0 +1,7 @@
+year = prompt("Enter your birth year ");
+ageInDays = (2021 - year) * 365;
+console.log(ageInDays);
+btn1 = document.getElementById("btn1");
+
+btn2 = document.getElementById("btn2");
+console.log(btn2);
